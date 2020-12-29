f6b8dcedd330e2ffc5ca344f7ec36ea7f2d5ba91
OsamahGharad <46979505+OsamahGharad@users.noreply.github.com>
2020-12-30 00:52:13 +0300

    save

--- src/App.js
+++ src/App.js
@@ -4,8 +4,14 @@ import Categories from './Categories';
 import items from './data';
 
 function App() {
+
+  const allCategories = ['all',...new Set(items.map((item) => item.category))];
+  console.log(allCategories);
+
   const [menuItems ,setMenuItems]= useState(items);
-  const [category ,setCategories]= useState([]);
+  const [categories ,setCategories]= useState(allCategories);
+
+  
 
   const filterItems = (category) =>{
     const newItems =menuItems.filter((item) => item.category ===category)
@@ -18,7 +24,7 @@ function App() {
       <h2>Our Menu</h2>
       <div className="underline"></div>
     </div>
-    <Categories filterItems ={filterItems}/>
+    <Categories categories={categories} filterItems ={filterItems}/>
     <Menu  items ={menuItems}/>
   </section>
 
